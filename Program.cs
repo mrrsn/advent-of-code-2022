d46492766896bdf8d2fa2800f8265a19e62d1e89
Rob Morrison <mrrsn@users.noreply.github.com>
2022-12-01 09:15:38 -0500

    Success

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace advent_of_code_2022
+{
+   internal class Program
+   {
+      private static void Main()
+      {
+         var lines = File.ReadAllLines(@"C:\Users\r.morrison\source\repos\advent-of-code-2022\Day01\input");
+
+         var sum = 0;
+         var sums = new List<int>();
+         foreach (var line in lines)
+         {
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+               sum += int.Parse(line);
+            }
+            else
+            {
+               sums.Add(sum);
+               sum = 0;
+            }
+         }
+
+         sums.Sort();
+         sums.Reverse();
+
+         Console.WriteLine(sums[2]+sums[1]+sums[0]);
+      }
+   }
+}
