a72bcc10a28136e359b2a06b48ec0f3d93bfaac5
Rob Morrison <mrrsn@live.com>
2022-12-10 23:54:58 -0500

    Day 8 Part 2 success

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -13,11 +13,15 @@ namespace advent_of_code_2022.Day08
          public bool visible_W;
          public bool visible_S;
          public bool visible_E;
+         public int view_N;
+         public int view_W;
+         public int view_S;
+         public int view_E;
       }
 
       public static void Main08()
       {
-         var lines = File.ReadAllLines(@"C:\Users\mrrsn\source\repos\advent-of-code-2022\Day08\test").ToList();
+         var lines = File.ReadAllLines(@"C:\Users\mrrsn\source\repos\advent-of-code-2022\Day08\input").ToList();
          var forestWidth = lines[0].Length;
          var forestHeight = lines.Count;
 
@@ -28,6 +32,7 @@ namespace advent_of_code_2022.Day08
             {
                forest[w, h].height = int.Parse( lines[h][w].ToString() );
                forest[w, h].visible_N = forest[w, h].visible_W = forest[w, h].visible_S = forest[w, h].visible_E = false;
+               forest[w, h].view_N = forest[w, h].view_W = forest[w, h].view_S = forest[w, h].view_E = 0;
 
                if ( w == 0 )
                   forest[w, h].visible_W = true;
@@ -50,6 +55,17 @@ namespace advent_of_code_2022.Day08
                   tallest_W = forest[w, h].height;
                   forest[w, h].visible_W = true;
                }
+
+               int scenic = 1;
+               for ( int i = w - 1; i > 0; i-- )
+               {
+                  if ( forest[i, h].height >= forest[w, h].height )
+                     break;
+
+                  scenic++;
+               }
+
+               forest[w, h].view_W = scenic;
             }
 
             int tallest_E = forest[forestWidth - 1, h].height;
@@ -60,6 +76,17 @@ namespace advent_of_code_2022.Day08
                   tallest_E = forest[w, h].height;
                   forest[w, h].visible_E = true;
                }
+
+               int scenic = 1;
+               for ( int i = w + 1; i < forestWidth - 1; i++ )
+               {
+                  if ( forest[i, h].height >= forest[w, h].height )
+                     break;
+
+                  scenic++;
+               }
+
+               forest[w, h].view_E = scenic;
             }
          }
 
@@ -73,6 +100,17 @@ namespace advent_of_code_2022.Day08
                   tallest_N = forest[w, h].height;
                   forest[w, h].visible_N= true;
                }
+
+               int scenic = 1;
+               for ( int i = h - 1; i > 0; i-- )
+               {
+                  if ( forest[w, i].height >= forest[w, h].height )
+                     break;
+
+                  scenic++;
+               }
+
+               forest[w, h].view_N = scenic;
             }
 
             int tallest_S = forest[w, forestHeight - 1].height;
@@ -83,6 +121,32 @@ namespace advent_of_code_2022.Day08
                   tallest_S = forest[w, h].height;
                   forest[w, h].visible_S = true;
                }
+
+               int scenic = 1;
+               for ( int i = h + 1; i < forestWidth - 1; i++ )
+               {
+                  if ( forest[w, i].height >= forest[w, h].height )
+                     break;
+
+                  scenic++;
+               }
+
+               forest[w, h].view_S = scenic;
+            }
+         }
+
+         for ( var h = 0; h < forestHeight; h++ )
+         {
+            for ( var w = 0; w < forestWidth; w++ )
+            {
+               if ( w == 0 )
+                  forest[w, h].view_W = 0;
+               if ( w == forestWidth - 1 )
+                  forest[w, h].view_E = 0;
+               if ( h == 0 )
+                  forest[w, h].view_N = 0;
+               if ( h == forestHeight - 1 )
+                  forest[w, h].view_S = 0;
             }
          }
 
@@ -92,16 +156,19 @@ namespace advent_of_code_2022.Day08
       private static void PrintResult( int forestWidth, int forestHeight, Tree[,] forest )
       {
          var visible = 0;
+         var scenicMax = 0;
          for ( var h = 0; h < forestHeight; h++ )
          {
             for ( var w = 0; w < forestWidth; w++ )
             {
                bool visibleAny = forest[w, h].visible_N || forest[w, h].visible_W || forest[w, h].visible_S || forest[w, h].visible_E;
                visible += visibleAny ? 1 : 0;
+               scenicMax = Math.Max( scenicMax, forest[w, h].view_N * forest[w, h].view_W * forest[w, h].view_S * forest[w, h].view_E );
             }
          }
 
          Console.WriteLine( "visible: " + visible );
+         Console.WriteLine( "scenic: " + scenicMax );
       }
    }
 }
