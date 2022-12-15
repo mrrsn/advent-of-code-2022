8a6dbaa7177c32c1e93fb10678cdb1057dd9cafc
Rob Morrison <mrrsn@live.com>
2022-12-14 22:34:29 -0500

    Day 9 Part 2 success

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 
@@ -7,14 +8,15 @@ namespace advent_of_code_2022.Day09
 {
    public class Day09
    {
-      private static List<string> log = new List<string>();
-      private static int Hx = 0; private static int Hy = 0; private static int Tx = 0; private static int Ty = 0;
+      private const int len = 10;
+      private static List<Point> log = new List<Point>();
+      private static Point[] rope = Enumerable.Repeat( new Point( 0, 0 ), len ).ToArray();
 
       public static void Main09()
       {
          var lines = File.ReadAllLines(@"C:\Users\mrrsn\source\repos\advent-of-code-2022\Day09\input").ToList();
 
-         Log();
+         log.Add( rope[len - 1] );
 
          lines.ForEach( line =>
          {
@@ -22,10 +24,16 @@ namespace advent_of_code_2022.Day09
             int i = int.Parse( move[1] );
             while ( i --> 0 )
             {
-               MoveHead( move[0][0] );
+               MoveHead( move[0][0], ref rope[0] );
 
-               while ( !AreBothWithin1() )
-                  MoveTail( move[0][0] );
+               for ( int n = 1; n < len; n++ )
+               {
+                  if ( rope[n - 1] != rope[n] )
+                  {
+                     MoveTail( rope[n - 1], ref rope[n] );
+                     log.Add( rope[len-1] );
+                  }
+               }
             }
          } );
 
@@ -33,31 +41,54 @@ namespace advent_of_code_2022.Day09
          Console.WriteLine( log.Count() );
       }
 
-      private static void MoveHead( char dir )
+      private static void MoveHead( char dir, ref Point h )
       {
-         if ( dir == 'R' || dir == 'L' )
-            Hx += dir == 'L' ? -1 : 1;
-         else
-            Hy += dir == 'D' ? -1 : 1;
+         switch ( dir )
+         {
+            case 'R':
+               h.X++;
+               break;
+            case 'L':
+               h.X--;
+               break;
+            case 'U':
+               h.Y++;
+               break;
+            case 'D':
+               h.Y--;
+               break;
+         }
       }
 
-      private static void MoveTail( char dir )
+      private static void MoveTail( Point h, ref Point t )
       {
-         if ( dir == 'R' || dir == 'L' )
+         if ( IsHead2StepsAwayInLine( h, t ) )
          {
-            Ty = Hy;
-            Tx += ( dir == 'L' ) ? -1 : 1;
+            if ( h.X > t.X )
+               t.X++;
+            else if ( h.X < t.X )
+               t.X--;
+            else if ( h.Y > t.Y )
+               t.Y++;
+            else if ( h.Y < t.Y )
+               t.Y--;
          }
-         else
+         else if ( AreHeadAndTailNotTouchingAndNotInLine( h, t ) )
          {
-            Tx = Hx;
-            Ty += ( dir == 'D' ) ? -1 : 1;
-         }
+            if ( h.X > t.X )
+               t.X++;
+            else if ( h.X < t.X )
+               t.X--;
 
-         Log();
+            if ( h.Y > t.Y )
+               t.Y++;
+            else if ( h.Y < t.Y )
+               t.Y--;
+         }
       }
 
-      private static void Log() => log.Add( Tx.ToString() + "," + Ty.ToString() );
-      private static bool AreBothWithin1() => Math.Abs( Hx - Tx ) <= 1 && Math.Abs( Hy - Ty ) <= 1;
+      private static bool AreTouching( Point h, Point t ) => Math.Abs( h.X - t.X ) <= 1 && Math.Abs( h.Y - t.Y ) <= 1;
+      private static bool IsHead2StepsAwayInLine( Point h, Point t ) => ( h.X == t.X && Math.Abs( h.Y - t.Y ) == 2 ) || ( h.Y == t.Y && Math.Abs( h.X - t.X ) == 2 );
+      private static bool AreHeadAndTailNotTouchingAndNotInLine( Point h, Point t ) => !AreTouching( h, t ) && !( h.X == t.X || h.Y == t.Y );
    }
 }
