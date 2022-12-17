11c19d5ec189d44d518dde45a13e4fbe7e43e28b
Rob Morrison <mrrsn@live.com>
2022-12-17 02:57:49 -0500

    Day 11 Part 2 success

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -6,10 +6,15 @@ using System.Linq;
 namespace advent_of_code_2022.Day11
 {
    public delegate long CalcDelegate( long x, long y );
+   public class Item
+   {
+      public int monkey;
+      public long value;
+   }
 
    public class Monkey
    {
-      public List<long> items;
+      public int monkeyNum;
       public CalcDelegate calc;
       public string right;
       public int test, t, f;
@@ -25,41 +30,41 @@ namespace advent_of_code_2022.Day11
       {
          var lines = File.ReadAllLines(@"C:\Users\mrrsn\source\repos\advent-of-code-2022\Day11\input").ToList();
          var chunks = new List<string>();
-         var monkeys = FillMonkeys( lines, chunks );
+         var items = new List<Item>();
+         var monkeys = FillMonkeys( lines, chunks, out items );
+         var common = 1;
+         monkeys.ForEach( m => common *= m.test );
 
-         var rounds = 20;
+         var rounds = 10000;
          while ( rounds --> 0 )
          {
             monkeys.ForEach( m =>
             {
-               m.items.ForEach( i =>
+               items.ForEach( i =>
                {
-                  i = m.calc.Invoke( i, m.right.Equals( "old" ) ? i : int.Parse( m.right ) );
-                  i /= 3;
-                  if ( i % m.test == 0 )
-                  {
-                     monkeys[m.t].items.Add( i );
-                  }
-                  else
-                  {
-                     monkeys[m.f].items.Add( i );
-                  }
+                  if ( i.monkey != m.monkeyNum ) return;
 
+                  i.value = m.calc.Invoke( i.value, m.right.Equals( "old" ) ? i.value : int.Parse( m.right ) );
+                  //i.value /= 3;
+                  i.monkey = i.value % m.test == 0 ? m.t : m.f;
+                  i.value %= common;
                   m.itemsHandled++;
                } );
-
-               m.items.Clear();
             } );
          }
 
+         Console.WriteLine();
+
          var counts = monkeys.Select( h => h.itemsHandled ).ToList();
          counts.Sort();
-
-         Console.WriteLine( "Part 1: " + counts[^2] * counts.Last() );
+         Console.WriteLine( counts[^2] * counts.Last() );
       }
 
-      private static List<Monkey> FillMonkeys( List<string> lines, List<string> chunks )
+      private static List<Monkey> FillMonkeys( List<string> lines, List<string> chunks, out List<Item> items )
       {
+         items = new List<Item>();
+         var tempItems = new List<Item>();
+
          for ( int i = 0; i < lines.Count - 6; )
          {
             chunks.Add( lines[i++].Trim().Split( ' ' )[1][0] + ";" +
@@ -75,12 +80,17 @@ namespace advent_of_code_2022.Day11
          chunks.ForEach( c =>
          {
             var m = c.Split( ';' );
-            var itemsStr = m[1];
-            var itemsList = itemsStr.Split( ", " ).ToList();
-            var itemsLongList = itemsList.Select( n => long.Parse( n ) ).ToList();
             var temp = new Monkey();
-            temp.items = new List<long>();
-            temp.items.AddRange( itemsLongList );
+            temp.monkeyNum = int.Parse( m[0] );
+            m[1].Split( ", " ).ToList().Select( n => int.Parse( n ) ).ToList().ForEach( i =>
+            {
+               var item = new Item
+               {
+                  monkey = temp.monkeyNum,
+                  value = i
+               };
+               tempItems.Add( item );
+            } );
             var op = m[2].Split( ' ' );
             temp.calc = op[1].Equals( "+" ) ? new CalcDelegate( Add ) : new CalcDelegate( Mult );
             temp.right = op[2];
@@ -89,6 +99,8 @@ namespace advent_of_code_2022.Day11
             temp.f = int.Parse( m[5] );
             monkeys.Add( temp );
          } );
+
+         items.AddRange( tempItems );
 
          return monkeys;
       }
